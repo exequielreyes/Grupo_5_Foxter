6d9e289ab2a77fbf39852c69e3fd393a3939fbcb
Misael Cortes <misael.cortes39@gmail.com>
2022-02-11 14:11:57 -0300

    Agrego productCart

--- Site/app.js
+++ Site/app.js
@@ -1,26 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-
-
+const path = require("path");
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
-const pathStatic = path.resolve(__dirname , "./public")
-app.use( express.static(pathStatic))
+const pathStatic = path.resolve(__dirname, "./public");
+app.use(express.static(pathStatic));
 
-app.listen(3000 , () => console.log('server running on port 3000'));
+app.listen(3000, () => console.log("server running on port 3000"));
 
-app.get('/' , (req , res) =>{
-    res.sendFile(path.join(__dirname , '/src/views/index.html'))
-})
-
-
-
-
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/views/index.html"));
+});
 
 //Login
-app.get('/login' , (req , res) =>{
-    res.sendFile(path.join(__dirname , '/src/views/usuario/login.html'))
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/views/usuario/login.html"));
+});
+
+//Product Cart
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/views/carrito/productCart.html"));
+});
