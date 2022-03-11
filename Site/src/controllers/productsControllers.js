ef56e47b27b7eb85af24142f72ec72a2f4106114
Soledad Alaniz <sole_lr@hotmail.com>
2022-03-11 07:13:05 -0300

    agregando filtros con diferentes categorias

--- Site/src/controllers/productsControllers.js
+++ Site/src/controllers/productsControllers.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { mainModule } = require("process");
+const { mainModule, nextTick } = require("process");
 
 
 const productsFilePath = path.join(__dirname, '../../data/products.json');
@@ -11,7 +11,24 @@ module.exports = {
     index: (req,res) => {
         res.render('products/productsList',{products});
     },
+    categoriaProducto: (req,res) => {
+        if( req.params.sexCategory){
+            category = req.params.category;
+            sexCategory = req.params.sexCategory;
 
+            let productsFilter=products.filter(product => product.category == category && product.sexCategory == sexCategory);
+
+            res.render('products/productsList',{'products':productsFilter});
+        }
+        else{
+            category = req.params.category;
+            let productsFilter=products.filter(product => product.category == category );
+
+            res.render('products/productsList',{'products':productsFilter});
+        }
+       
+        
+    },
     detalleProducto: (req, res) => {
         id = req.params.id;
         let product = products.find(product => product.id == id);
--- Site/src/routes/productsRoutes.js
+++ Site/src/routes/productsRoutes.js
@@ -7,5 +7,8 @@ router.get('/', productsControllers.index);
 router.get('/detail/:id' , productsControllers.detalleProducto);
 router.get('/edit/:id' , productsControllers.editarProducto);
 router.get('/cart' , productsControllers.carrito);
+router.get('/:category', productsControllers.categoriaProducto);
+router.get('/:category/:sexCategory', productsControllers.categoriaProducto);
+
 
 module.exports = router;
