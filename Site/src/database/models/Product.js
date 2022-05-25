053796b184bab3ccd769930f28cab82f555a97ba
Misael Cortes <misael.cortes39@gmail.com>
2022-05-25 16:16:36 -0300

    Creacion de modelos

---
+++ Site/src/database/models/Product.js
@@ -0,0 +1,81 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, dataTypes) => {
+
+
+let alias = 'Product';
+
+let cols = {
+    idProduct: {
+        type: dataTypes.INTEGER(10).UNSIGNED,
+        primeryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: dataTypes.STRING(100),
+        allowNull: false
+    },
+    price: {
+        type: dataTypes.INTEGER(10),
+        allowNull: false
+    },
+    discount: {
+        type: dataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+    },
+    idCategory: dataTypes.INTEGER(10).UNSIGNED,
+    description: {
+        type: dataTypes.STRING(255),
+    },
+    idSexCategory: dataTypes.INTEGER(10).UNSIGNED,
+    idSaleCategory: dataTypes.INTEGER(10).UNSIGNED
+}
+
+let config = {
+    timestamps: false
+}
+
+const Product = sequelize.difine(alias, cols, config);
+
+//Relaciones
+
+Product.associate = (models) => {
+    Product.belongsTo(models.Categorie, {
+        as: 'categorie',
+        foreignKey: 'idCategory'
+    }),
+
+    Product.belongsTo(models.saleCategory, {
+        as: 'saleCategory',
+        foreignKey: 'idSaleCategory'
+    }),
+
+    Product.belongsTo(models.sexCategory, {
+        as: 'sexCategory',
+        foreignKey: 'idSexCategory'
+    }),
+
+    Product.hasMany(models.Image, {
+        as: 'images',
+        foreignKey: 'productId'
+    }),
+
+    Product.belongsToMany(models.Size, {
+        as: 'sizes',
+        through: 'productsSizes',
+        foreignKey: 'productsId',
+        otherKey: 'sizeId'
+    }),
+
+    Product.belongsToMany(models.User, {
+        as: 'users',
+        through: 'userProduct',
+        foreignKey: 'productId',
+        otherKey: 'userId'
+    })
+
+}
+
+return Product;
+}
