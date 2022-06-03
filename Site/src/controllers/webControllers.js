89d2be667178609e242c6a4a2fd8c05f8b30f09a
Misael Cortes <misael.cortes39@gmail.com>
2022-06-03 02:10:00 -0300

    Actualizacion webController index con db

--- Site/src/controllers/webControllers.js
+++ Site/src/controllers/webControllers.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require('fs');
 
 const { mainModule, nextTick } = require("process");
+const db = require("../database/models");
 
 
 const productsFilePath = path.join(__dirname, '../../data/products.json');
@@ -10,7 +11,14 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 module.exports = {
     index: (req, res) => {
-        res.render("index",{products});
+
+		db.Product.findAll({
+			include: [{association: "saleCategory"},{association: "images"},{association: "category"}]
+		})
+		.then((products) => {
+			  res.render("index",{products});
+		})
+      
     },
   
     search: (req, res) => {
