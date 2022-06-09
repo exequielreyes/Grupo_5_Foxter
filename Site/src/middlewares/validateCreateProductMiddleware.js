e326ec07612e70cf4bd79e20541035c48bba8eda
exequielreyes <exequielreyes93@gmail.com>
2022-06-09 20:25:21 -0300

    Validacion en el producto por backend

--- Site/src/middlewares/validateCreateProductMiddleware.js
+++ Site/src/middlewares/validateCreateProductMiddleware.js
@@ -1,8 +1,16 @@
 const { body } = require('express-validator');
 
 const validations= [
-    body('name').notEmpty().withMessage('debe completar este campo'),
-    body('price').notEmpty().withMessage('debe completar este campo'),
+    body('name').notEmpty().withMessage('debe completar este campo').bail()
+    .isLength({min: 5 }).withMessage('El campo nombre debe tener al menos 5 caracteres'),
+
+    body('description').notEmpty().withMessage('debe completar este campo').bail()
+    .isLength({min: 20 }).withMessage('El campo descripcion debe tener al menos 20 caracteres'),
+
+    body('price').notEmpty().withMessage('debe completar este campo').bail()
+    .isNumeric().isLength({ min: 0}),
+
+
    
 ]
 module.exports =  validations;
