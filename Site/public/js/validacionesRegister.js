9991be11f4ad15973beb57051da4e9a9917eaac6
exequielreyes <exequielreyes93@gmail.com>
2022-06-14 19:57:24 -0300

    Validacion campo  apellido

--- Site/public/js/validacionesRegister.js
+++ Site/public/js/validacionesRegister.js
@@ -5,12 +5,38 @@ let formulario = document.querySelector(".form");
 formulario.addEventListener("submit" , (e) =>{
 e.preventDefault();
 
+let errores = [];
+
 let campoNombre = document.querySelector(".form #name");
+let campoApellido = document.querySelector(".form #lastName")
 
 if (campoNombre.value == "") {
-    alert("El campo de nombre tiene que estar completo");
+    errores.push("El campo de nombre tiene que estar completo");
+    return campoNombre;
 }else if (campoNombre.value.length < 3) {
-    alert("El campo de nombre debe tener al menos 3 caracteres")
+    errores.push("El campo de nombre debe tener al menos 3 caracteres")
+}
+
+
+if (campoApellido.value == "") {
+    errores.push("El campo de apellido tiene que estar completo");
+    return campoApellido;
+}else if (campoApellido.value.length < 3) {
+    errores.push("El campo de apellido debe tener al menos 3 caracteres")
+}
+
+
+
+
+if (errores.length > 0) {
+    e.preventDefault();
+
+    let ulErrores = document.querySelector("div.errores ul");
+    for (let i = 0; i < errores.length; i++) {
+        
+        ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+    }
+
 }
 
 })
