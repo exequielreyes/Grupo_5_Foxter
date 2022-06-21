747b63ae99ce9dfabc75ec17440696985232e40e
Soledad Alaniz <sole_lr@hotmail.com>
2022-06-21 19:10:45 -0300

    actualizo middleware de categorias

--- Site/src/middlewares/categoriasMiddleware.js
+++ Site/src/middlewares/categoriasMiddleware.js
@@ -1,8 +1,8 @@
 const db = require("../database/models");
 
-module.exports = categoriasMiddleware = (req, res, next) => {
-    if (res.locals.categorias==undefined) {
-        db.Category.findAll()
+module.exports =  categoriasMiddleware = async (req, res, next) => {
+    if (!res.locals.categorias) {
+        await db.Category.findAll()
         .then(todascategorias => {
             res.locals.categorias = todascategorias;
         })
