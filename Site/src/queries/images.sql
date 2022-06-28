56059f852d7e9ea68a57c5b9de1df22bdb4083e9
exequielreyes <exequielreyes93@gmail.com>
2022-06-28 16:49:10 -0300

    sql tables

---
+++ Site/src/queries/images.sql
@@ -0,0 +1,27 @@
+/*
+-- Query: SELECT * FROM foxter_db.images
+LIMIT 0, 1000
+
+-- Date: 2022-06-28 16:47
+*/
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (23,'1656442237693-Buzo.jpg',4);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (24,'1656442237762-Buzo2.jpg',4);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (25,'1656442598955-BuzoLoto.jpg',22);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (26,'1656442599061-BuzoLoto2.jpg',22);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (27,'1656442666097-Calza2.jpg',23);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (28,'1656442666032-Calza.jpg',23);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (29,'1656442737089-pantalon.jpg',24);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (30,'1656442737254-pantalon2.jpg',24);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (31,'1656442792138-Calza3.jpg',25);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (32,'1656442792255-calza4.jpg',25);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (33,'1656442792312-calza5.jpg',25);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (34,'1656442910599-Malla.jpg',26);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (35,'1656442910879-Malla2.jpg',26);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (36,'1656443010579-Remeras.jpg',27);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (37,'1656443010632-Remeras2.jpg',27);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (38,'1656443100990-Campera.jpg',28);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (39,'1656443101047-campera2.jpg',28);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (40,'1656443162905-pantalonUnisex.jpg',29);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (41,'1656443163097-PantalonUnisex2.jpg',29);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (42,'1656443240517-musculosa.jpg',30);
+INSERT INTO `` (`idImage`,`name`,`productId`) VALUES (43,'1656443240606-musculosa2.jpg',30);
---
+++ Site/src/queries/sizes.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM foxter_db.sizes
+LIMIT 0, 1000
+
+-- Date: 2022-06-28 16:48
+*/
+INSERT INTO `` (`idSize`,`name`) VALUES (1,'S');
+INSERT INTO `` (`idSize`,`name`) VALUES (2,'M');
+INSERT INTO `` (`idSize`,`name`) VALUES (3,'L');
+INSERT INTO `` (`idSize`,`name`) VALUES (4,'XL');
+INSERT INTO `` (`idSize`,`name`) VALUES (5,'XXL');
