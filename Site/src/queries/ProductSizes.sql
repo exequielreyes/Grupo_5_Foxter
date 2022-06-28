e26f7826b94a796c41fe6f0442bf3f3ac12d3de7
exequielreyes <exequielreyes93@gmail.com>
2022-06-28 16:58:57 -0300

    tabla productSizes

---
+++ Site/src/queries/ProductSizes.sql
@@ -0,0 +1,32 @@
+/*
+-- Query: SELECT * FROM foxter_db.productssizes
+LIMIT 0, 1000
+
+-- Date: 2022-06-28 16:58
+*/
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (1,2,4);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (22,1,22);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (23,4,22);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (24,2,23);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (25,3,23);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (26,1,24);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (27,2,24);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (28,1,25);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (29,2,25);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (30,3,25);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (31,3,26);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (32,4,26);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (33,5,26);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (34,1,27);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (35,2,27);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (36,3,27);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (37,2,28);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (38,3,28);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (39,4,28);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (40,1,29);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (41,2,29);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (42,3,29);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (43,4,29);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (44,1,30);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (45,2,30);
+INSERT INTO `` (`idsizeProduct`,`sizeId`,`productsId`) VALUES (46,3,30);
