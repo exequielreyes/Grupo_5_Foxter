17bfee54ba15169fc21b96141a3e96b272f5437f
Misael Cortes <misael.cortes39@gmail.com>
2022-06-27 23:43:50 -0300

    implementacion api productos en dashboard

--- dashboard/src/components/ContenRowTop.js
+++ dashboard/src/components/ContenRowTop.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useState, useEffect } from "react"
 import CategoriInDb from "./CategoriaInDb";
 // import LastProductInDb from "./LastProductInDb";
 // import LastUserInDb from "./LastUserInDb";
@@ -7,30 +7,43 @@ import ProductList from "./ProductList";
 import ContentRow from "./ContentRow";
 
 
+
 function ContenRowTop(props) {
+    
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+    const fetchData = async () => {
+        const res = await fetch(
+            "http://localhost:3000/api/products",
+        );
+        const data = await res.json();
+        setData(data);        
+    };
+    fetchData();
+}, []);
+
     return (
         <div id="content-wrapper" className="d-flex flex-column">
 
 
 
             <div class="container-fluid">
-                <ContentRow />
+                <ContentRow dataProduct = {data} />
+                
                 <div className="row">
                     
                     {/* <LastProductInDb /> */}
                     {/* <LastUserInDb /> */}
-                    <CategoriInDb />
-                    <ProductList />
+                    <CategoriInDb categories = {data.countByCategory} />
+                    <ProductList products = {data.products} />
                 </div>
             </div>
 
 
 
         </div>
-
-
-
-
 
     );
 }
